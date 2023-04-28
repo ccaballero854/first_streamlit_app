4a28f31113613d57466229454a5292b5c7f294b4
ccaballero854 <131801383+ccaballero854@users.noreply.github.com>
2023-04-28 16:58:56 -0500

    Update streamlit_app.py
    removed stop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,13 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import pandas
@@ -41,8 +43,6 @@ try:
 except URLError as e:
     streamlit.error()
     
-# don't run anything past here while we troubleshoot
-streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
